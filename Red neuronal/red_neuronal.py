f80ec55ad5d3d59edfa0cc36ced771fc8ff5750e
TheIcySpark <saacmanjarrez@gmail.com>
2020-12-11 13:03:09 -0600

    forward pass

--- Red neuronal/red_neuronal.py
+++ Red neuronal/red_neuronal.py
@@ -1,93 +1,48 @@
 import math
 
-class Red_neuronal:
-    def __init__ (self, descripcion_capas, descripcion_bias, descripcion_pesos):
-        self.CAPAS = 0
-        self.BIAS = 1
-        self.PESOS = 2
-        self.ERRORES = 3
-        self.red = [[], [[]], [], []]
-        self.red[self.ERRORES] = [0] * descripcion_capas[-1]
+
+class RedNeuronal:
+    def __init__(self, neuronas_entrada, descripcion_capas, descripcion_bias, descripcion_conexiones):
+        descripcion_conexiones.insert(0, [])
+        descripcion_bias.insert(0, [])
+        self.red_entradas = []
+        self.red_salidas = []
         i = 0
         while i < len(descripcion_capas):
-            self.red[self.CAPAS].append([])
-            self.red[self.CAPAS][i] = [0] * descripcion_capas[i]
+            self.red_entradas.append([0] * descripcion_capas[i])
+            self.red_salidas.append([0] * descripcion_capas[i])
             i += 1
-        i = 0
-        while i < len(descripcion_bias):
-            self.red[self.BIAS].append([])
-            self.red[self.BIAS][i + 1] = descripcion_bias[i]
-            i += 1
-        i = 0
-        while i < len(descripcion_pesos):
-            self.red[self.PESOS].append([])
-            self.red[self.PESOS][i] = descripcion_pesos[i]
-            i += 1
-    
+        self.red_salidas[0] = neuronas_entrada
+        self.red_conexiones = descripcion_conexiones
+        self.red_bias = descripcion_bias
 
-    def suma_sinaptica(self, capa, neurona):
-        i = 0
+    
+    def suma_conexion_sinaptica(self, capa, neurona):
         ret = 0.0
-        capa -= 1
-        while i < len(self.red[self.CAPAS][capa]):
-            ret += self.red[self.CAPAS][capa][i] * self.red[self.PESOS][capa][i][neurona]
-            i += 1
-        ret += self.red[self.BIAS][capa + 1][neurona]
+        neurona_conectada = 0
+        while neurona_conectada < len(self.red_entradas[capa - 1]):
+            ret += self.red_conexiones[capa][neurona][neurona_conectada] * self.red_salidas[capa - 1][neurona_conectada] 
+            neurona_conectada += 1
+        ret += self.red_bias[capa][neurona]
         return ret
 
 
     def prediccion(self):
         capa = 1
-        while capa < len(self.red[self.CAPAS]):
+        while capa < len(self.red_entradas):
             neurona = 0
-            while neurona < len(self.red[self.CAPAS][capa]):
-                self.red[self.CAPAS][capa][neurona] = funcion_activacion(self.suma_sinaptica(capa, neurona))
+            while neurona < len(self.red_entradas[capa]):
+                self.red_entradas[capa][neurona] = self.suma_conexion_sinaptica(capa, neurona)
+                self.red_salidas[capa][neurona] = funcion_activacion(self.red_entradas[capa][neurona])
                 neurona += 1
             capa += 1
-    
 
-    def calculo_errores(self, resultados_esperados):
-        neurona = 0
-        while neurona < len(self.red[self.ERRORES]):
-            self.red[self.ERRORES][neurona] = math.pow(resultados_esperados[neurona] -
-                    self.red[self.CAPAS][-1][neurona], 2) / 2
-            neurona += 1
-    
-
-    def backpropagation(self, resultados_esperados):
-        taza_aprendisaje = 0.5
-        neurona = 0
-        while neurona < len(self.red[self.PESOS][1]):
-            neurona_b = 0
-            while neurona_b < len(self.red[self.PESOS][1][neurona]):
-                d_total, d_neurona, d_peso, derivada  = 0.0, 0.0, 0.0, 0.0
-                d_total = self.red[self.CAPAS][-1][neurona_b] - resultados_esperados[neurona_b]
-                d_neurona = self.red[self.CAPAS][-1][neurona_b] * (1 - self.red[self.CAPAS][-1][neurona_b])
-                d_peso = self.red[self.CAPAS][1][neurona]
-                derivada = d_total * d_neurona * d_peso
-                self.red[self.PESOS][1][neurona][neurona_b] = \
-                    self.red[self.PESOS][1][neurona][neurona_b] - taza_aprendisaje * derivada
-                neurona_b += 1
-            neurona += 1
-        print(self.red[self.PESOS])
-
-
-    def entrenamiento(self, inputs,  resultados_esperados):
-        neurona = 0
-        while neurona < len(self.red[self.CAPAS][0]):
-            self.red[self.CAPAS][0][neurona] = inputs[neurona]
-            neurona += 1
-        self.prediccion()
-        self.calculo_errores(resultados_esperados)
-        self.backpropagation(resultados_esperados)
-
-
-
+        
 def funcion_activacion(x):
     return 1 / (1 + math.exp(-x))
 
-    
+
 if __name__ == "__main__":
-    red = Red_neuronal([2,2,2], [[0.35, 0.35], [0.6, 0.6]], 
-    [[[0.15, 0.25], [0.2, 0.3]], [[0.4, 0.5], [0.45, 0.55]]])
-    red.entrenamiento([0.05, 0.1], [0.01, 0.99])
+    red = RedNeuronal([0.05, 0.1], [2, 2, 2], [[0.35, 0.35], [0.6, 0.6]], 
+            [[[0.15, 0.2], [0.25, 0.3]], [[0.40, 0.45], [0.50, 0.55]]])
+    red.prediccion()
